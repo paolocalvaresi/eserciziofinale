a1abbf9fafbbdffa79e45da0931c27dbd6040223
paolocalvaresi <paolo.calvaresi.v@gmail.com>
2017-03-07 15:26:29 +0100

    server della index

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 const PORT = 3000;
+
+//SERVO LA INDEX.HTML
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, "..", "client", "index.html"));
+});
 
 
 app.listen(PORT, function () {
